df142c73a2f1802b6d371c8d65e93fdaca18e1d8
Ariane Deschenes Foucher <ariane.df@me.com>
2016-03-22 15:50:28 -0400

    Did it again using iterator instead of method. It works as well.

--- exercise4.rb
+++ exercise4.rb
@@ -1,4 +1,22 @@
-def bitmaker(x)
+# def bitmaker(x)
+#   if (x % 3 == 0) && (x % 5 == 0)
+#     puts "Bitmaker"
+#   elsif x % 3 == 0
+#     puts "Bit"
+#   elsif x % 5 == 0
+#     puts "Maker"
+#   else
+#     puts x
+#   end
+# end
+#
+# n = 1
+# until n == 100
+# puts bitmaker(n)
+# n += 1
+# end
+
+(1..100).each do |x|
   if (x % 3 == 0) && (x % 5 == 0)
     puts "Bitmaker"
   elsif x % 3 == 0
@@ -8,10 +26,4 @@ def bitmaker(x)
   else
     puts x
   end
-end
-
-n = 1
-until n == 100
-puts bitmaker(n)
-n += 1
-end
+end    
