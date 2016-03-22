db77eb36f9d873699e6379623f2f5c395c12942d
Ariane Deschenes Foucher <ariane.df@me.com>
2016-03-22 15:11:40 -0400

    asked for user's name and puts birth year

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,6 @@
 puts "What is your name?"
 name = gets.chomp
 puts "Hi #{name}!"
+puts "How old are you, #{name}?"
+age = gets.chomp
+puts "You are #{age} years old, that means you were born in #{2016 - (age.to_i)}!"
